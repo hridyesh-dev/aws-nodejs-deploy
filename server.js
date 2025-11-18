8d3213fabe28112518b6fed3ea833127bbe90002
hridyesh-dev <hridyesh.dev@gmail.com>
2025-11-18 14:46:20 +0530

    added user details

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get("/api/get",(req,res)=>{
         message:"Hello back to node js "
     })
 })
+app.get("/api/get_user_details",(req,res)=>{
+    res.send({
+        user:{
+           name:"hri",
+           age:22,
+           contact:1234 
+        }
+    })
+})
 
 app.listen(8000,()=>{
     console.log("listening to port 8000");
