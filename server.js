3f85b519dc9aca4c703c6d7f8a857eff75e74e68
hridyesh-dev <hridyesh.dev@gmail.com>
2025-11-18 15:22:32 +0530

    added .env vars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const app=express();
-
+require("dotenv").config
 app.get("/api/get",(req,res)=>{
     res.send({
         message:"Hello back to node js "
@@ -9,13 +9,13 @@ app.get("/api/get",(req,res)=>{
 app.get("/api/get_user_details",(req,res)=>{
     res.send({
         user:{
-           name:"hri",
-           age:22,
-           contact:1234 
-        }
+            name:"hri",
+            age:22,
+            contact:1234 
+        },env:process.env.NAME
     })
 })
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("listening to port 8000");
 })
